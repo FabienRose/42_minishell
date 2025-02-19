c2c10d1ef58aae8509b8cdec0dbc68cee7a1a574
KRASGauthier <carlmc.kayre@gmail.com>
2025-02-19 11:02:58 +0100

    WORK: Test branch 2

--- minishell.c
+++ minishell.c
@@ -6,11 +6,11 @@
 /*   By: kgauthie <kgauthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 10:53:27 by fmixtur           #+#    #+#             */
-/*   Updated: 2025/02/19 11:00:29 by kgauthie         ###   ########.fr       */
+/*   Updated: 2025/02/19 11:02:26 by kgauthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"                                                                                   (_____)
+#include "shell.h"
 
 int	main(int argc, char **argv)
 {
