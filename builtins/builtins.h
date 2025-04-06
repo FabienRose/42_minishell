e8c0ac310e5ed28df9960ab086e4fa8cff837b3f
KRASGauthier <carlmc.kayre@gmail.com>
2025-04-06 10:28:20 +0200

    WORK: Var set on command done (not in builtin)

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -6,7 +6,7 @@
 /*   By: kgauthie <kgauthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:00:55 by fmixtur           #+#    #+#             */
-/*   Updated: 2025/03/30 21:56:31 by kgauthie         ###   ########.fr       */
+/*   Updated: 2025/04/06 10:19:54 by kgauthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ typedef struct s_shell	t_shell;
 t_bool	change_directory(t_shell *shell, char **arg);
 t_bool	blt_echo(char **args);
 t_bool	pwd(char **arguments);
+t_bool	add_env(t_shell *shell, char *arg);
 t_bool	export(t_shell *shell, char **arg);
 void	bubble_sort(char **array);
 t_bool	is_valid_identifier(char *str);
